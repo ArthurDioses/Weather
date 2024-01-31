17262bee341cfcdf3feeab4c149ff5ec4fb20e58
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-01-31 00:27:19 -0500

    Impl() - Update compileSdk and targetSdk to 34 - Enable DataBinding - Add dependency viewmodel - Add dependency coroutines - Add dependency livedata - Add dependency kotlin activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dioses.weather"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dioses.weather"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,14 +33,39 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    dataBinding {
+        enable = true
+    }
 }
+
+val androidx_core_version = "1.12.0"
+val appcompat_version = "1.6.1"
+val material_version = "1.11.0"
+val constraint_version = "2.1.4"
+val lifecycle_version = "2.7.0"
+val coroutines_version = "1.7.1"
+val activity_version = "1.8.2"
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:$androidx_core_version")
+    implementation("androidx.appcompat:appcompat:$appcompat_version")
+    implementation("com.google.android.material:material:$material_version")
+    implementation("androidx.constraintlayout:constraintlayout:$constraint_version")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+
+    //LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Kotlin-Activity
+    implementation("androidx.activity:activity-ktx:$activity_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
