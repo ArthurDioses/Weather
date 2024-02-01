9fe00e77e1fe31593f9ade03316d4b7804ac779d
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-02-01 09:54:05 -0500

    Impl() - Add model repository

---
+++ app/src/main/java/com/dioses/weather/mainModule/model/MainRepository.kt
@@ -0,0 +1,16 @@
+package com.dioses.weather.mainModule.model
+
+import com.dioses.weather.common.entities.WeatherForecastEntity
+
+class MainRepository {
+    private val remoteDatabase = RemoteDatabase()
+
+    suspend fun getWeatherAndForecast(
+        lat: Double,
+        lon: Double,
+        appId: String,
+        units: String,
+        lang: String
+    ): WeatherForecastEntity =
+        remoteDatabase.getWeatherForecastByCoordinates(lat, lon, appId, units, lang)
+}
