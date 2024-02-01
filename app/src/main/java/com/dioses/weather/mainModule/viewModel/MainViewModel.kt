8bb53703dcde937ee23d22c620dc50bfba722baa
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-02-01 18:57:25 -0500

    Impl() - Add ViewModel

---
+++ app/src/main/java/com/dioses/weather/mainModule/viewModel/MainViewModel.kt
@@ -0,0 +1,43 @@
+package com.dioses.weather.mainModule.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dioses.weather.R
+import com.dioses.weather.common.entities.WeatherForecastEntity
+import com.dioses.weather.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<WeatherForecastEntity>()
+    fun getResult(): LiveData<WeatherForecastEntity> = result
+
+    private val snackBarMsg = MutableLiveData<Int>()
+    fun getSnackBarMsg() = snackBarMsg
+
+    private val loaded = MutableLiveData<Boolean>()
+    fun isLoaded() = loaded
+
+    suspend fun getWeatherAndForecast(
+        lat: Double,
+        lon: Double,
+        appId: String,
+        units: String,
+        lang: String
+    ) {
+        viewModelScope.launch {
+            try {
+                loaded.value = false
+                val resultServer = repository.getWeatherAndForecast(lat, lon, appId, units, lang)
+                result.value = resultServer
+            } catch (e: Exception) {
+                snackBarMsg.value = R.string.main_error_server
+            } finally {
+                loaded.value = true
+            }
+        }
+    }
+}
