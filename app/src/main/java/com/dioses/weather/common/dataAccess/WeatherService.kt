ba6a1adee023780d9dddd2445d3fb070b6016faf
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-02-01 00:29:45 -0500

    Impl() - Add Constants and service retrofit

---
+++ app/src/main/java/com/dioses/weather/common/dataAccess/WeatherService.kt
@@ -0,0 +1,19 @@
+package com.dioses.weather.common.dataAccess
+
+import com.dioses.weather.common.WeatherForecastEntity
+import com.dioses.weather.common.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET(Constants.ON_CALL_PATH)
+    suspend fun getWeatherForecastByCoordinates(
+        @Query(Constants.LATITUDE_PARAM) lat: Double,
+        @Query(Constants.LONGITUDE_PARAM) long: Double,
+        @Query(Constants.APP_ID_PARAM) appId: String,
+        @Query(Constants.UNITS_PARAM) units: String,
+        @Query(Constants.LANGUAGE_PARAM) lang: String
+    ): WeatherForecastEntity
+
+
+}
