c4cb02087dbe7fc69cf0ac1e90fd004fdd6adef9
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-02-02 02:09:51 -0500

    Impl() - DataBinding: Advanced format

--- app/src/main/java/com/dioses/weather/common/utils/CommonUtils.kt
+++ app/src/main/java/com/dioses/weather/common/utils/CommonUtils.kt
@@ -1,5 +1,6 @@
 package com.dioses.weather.common.utils
 
+import com.dioses.weather.common.entities.Weather
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -10,5 +11,15 @@ object CommonUtils {
 
     private fun getFormatedTime(epoch: Long, pattern: String): String {
         return SimpleDateFormat(pattern, Locale.getDefault()).format(epoch * 1_000)
+    }
+
+    fun getWeatherMain(weather: List<Weather>?): String {
+        return if (!weather.isNullOrEmpty()) weather[0].main
+        else "-"
+    }
+
+    fun getWeatherDescription(weather: List<Weather>?): String {
+        return if (!weather.isNullOrEmpty()) weather[0].description
+        else "-"
     }
 }
