541195802b291161bf5848a00262df03ecd399f9
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-02-01 19:19:12 -0500

    Impl() - View(context)

--- app/src/main/java/com/dioses/weather/mainModule/view/MainActivity.kt
+++ app/src/main/java/com/dioses/weather/mainModule/view/MainActivity.kt
@@ -2,11 +2,53 @@ package com.dioses.weather.mainModule.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.dioses.weather.BR
 import com.dioses.weather.R
+import com.dioses.weather.databinding.ActivityMainBinding
+import com.dioses.weather.mainModule.viewModel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupViewModel()
+        setupObservers()
+    }
+
+    private fun setupViewModel() {
+        val vm: MainViewModel by viewModels()
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, vm)
+
+    }
+
+    private fun setupObservers() {
+        binding.viewModel?.let {
+            it.getSnackBarMsg().observe(this) { resMsg ->
+                Snackbar.make(binding.root, resMsg, Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope.launch {
+            binding.viewModel.getWeatherAndForecast(
+                -4.9082,
+                -80.7195,
+                "add22ba261f99bc48cc324286d384c78",
+                "metric",
+                "en"
+            )
+        }
     }
 }
